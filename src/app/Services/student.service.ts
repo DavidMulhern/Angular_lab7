79c5a3698ee72c48836c644437c359756bd2dc56
David Mulhern <dave_mulhern1@hotmail.com>
2021-03-10 15:25:12 +0000

    Lab7 error outstanding

---
+++ src/app/Services/student.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable, observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  constructor(private http:HttpClient) { }
+
+  GetStudentData():Observable<any>{
+    return this.http.get('https://jsonblob.com/api/jsonblob/6981595b-81aa-11eb-8c36-75d2800c5526')
+  }
+
+  GetWeatherData():Observable<any>{
+    return this.http.get('http://api.openweathermap.org/data/2.5/weather?q=Galway&APPID=6a66416403ed8e5e6e762cb8c261f303')
+  }
+}
